3b8bd376826be8101b9a8c3fd6904ae9fd8a6875
Gustavo Camargo <gustavoscamargo@outlook.com>
2023-11-22 16:00:50 +0000

    Updated sign_up and added car creation

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -13,12 +13,16 @@ class CarsController < ApplicationController
 
   def new
     @car = Car.new
-    render :new
   end
 
   def create
-    @car = Car.create(car_params)
-    redirect_to car_path(@car)
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
