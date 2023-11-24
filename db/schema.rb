10b8200d85824640020e60e31b538775bdf06c61
Gustavo Camargo <gustavoscamargo@outlook.com>
2023-11-24 17:03:27 +0000

    bugs destroy and my_cars fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_141020) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_115056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,8 +53,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_141020) do
     t.string "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
