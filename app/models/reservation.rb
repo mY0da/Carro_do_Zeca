fedc352928227a2fcb80705bc9837654eee15e5b
Martim Costa <martimcosta95@gmail.com>
2023-11-24 16:09:24 +0000

    a lot of changes

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,9 +9,9 @@ class Reservation < ApplicationRecord
   private
 
   def dates_are_valid
-    errors.add(:start_date, 'Cannot be in the past.') if start_date.present? && start_date < Date.today
-    errors.add(:end_date, 'Cannot be in the past.') if end_date.present? && end_date < Date.today
-    errors.add(:end_date, 'Must be after start date') if start_date.present? && end_date.present? && start_date > end_date
+    errors.add(:start_date, 'Cannot be in the past.') if start_date.present? && Date.parse(start_date) < Date.today
+    errors.add(:end_date, 'Cannot be in the past.') if end_date.present? && Date.parse(end_date) < Date.today
+    errors.add(:end_date, 'Must be after start date') if start_date.present? && end_date.present? && Date.parse(start_date) > Date.parse(end_date)
   end
 
 end
